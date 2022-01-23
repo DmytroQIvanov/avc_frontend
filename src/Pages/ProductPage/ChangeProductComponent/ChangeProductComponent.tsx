1687ba9f44e29069734a102e46fccb542c650e14
DmytroQIvanov <20IvanovDmitriy03@gmail.com>
2022-01-23 17:57:24 +0200

    added: styles

--- src/Pages/ProductPage/ChangeProductComponent/ChangeProductComponent.tsx
+++ src/Pages/ProductPage/ChangeProductComponent/ChangeProductComponent.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 
 import styles from "./ChangeProduct.module.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../../store/store";
 
 type ChangeProductComponent = {
   children: (selectedPanel: number) => React.ReactNode;
@@ -10,11 +12,17 @@ const ChangeProductComponent: React.FC<ChangeProductComponent> = ({
   children,
 }) => {
   const [selectedPanelIndex, setSelectedIndex] = useState(0);
+  const commentsNumber = useSelector(
+    (state: RootState) => state.product?.product?.comments.length
+  );
   return (
     <div className={styles.changeProductContent}>
       <div className="changeProductContent">
         <span onClick={() => setSelectedIndex(0)}>Опис товару</span>
-        <span onClick={() => setSelectedIndex(1)}>Відгуки</span>
+        <span onClick={() => setSelectedIndex(1)}>
+          Відгуки{" "}
+          {commentsNumber && commentsNumber >= 1 ? `(${commentsNumber})` : ""}
+        </span>
       </div>
       <div className={styles.changeProductContent_content}>
         {children(selectedPanelIndex)}
